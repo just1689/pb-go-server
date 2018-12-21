e054390b38e5148d89d0c4cf93358a3d55c9facc
Just1689 <Just1689@gmail.com>
2018-12-21 22:24:09 +0200

    Added file

---
+++ data/1.download-file.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget https://storage.googleapis.com/pb-sql/parabible_data.sql -O sql/parabible_data.sql
--- data/create-docker-image.sh
+++ data/create-docker-image.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/pb-sql/parabible_data.sql -O sql/parabible_data.sql
 docker build -t pb/pb:dev .
