962d808b7c27aafcfec0f845805ac5d48f15d297
Just1689 <Just1689@gmail.com>
2018-12-29 17:26:12 +0200

    Clean up

--- app/api/api.go
+++ app/api/api.go
@@ -69,15 +69,3 @@ func HandleTermSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("The square number of 1 is: %d", squareNum)
 
 }
-
-func HandleWordLookup(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func HandleChapterText(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func HandleNodeText(w http.ResponseWriter, r *http.Request) {
-
-}
