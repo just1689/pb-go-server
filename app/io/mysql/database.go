9a6c9b60e0218725761325695b59720906201de3
Just1689 <Just1689@gmail.com>
2018-12-29 18:50:24 +0200

    Refactor

--- app/io/mysql/database.go
+++ app/io/mysql/database.go
@@ -2,8 +2,10 @@ package mysql
 
 import "database/sql"
 
+const DefaultConnection = "root:toor@tcp(127.0.0.1:3306)/parabible_test"
+
 func Open(connection string) (*sql.DB, error) {
-	return sql.Open("mysql", "root:toor@tcp(127.0.0.1:3306)/parabible_test")
+	return sql.Open("mysql", connection)
 }
 
 func Connect(db *sql.DB) error {
